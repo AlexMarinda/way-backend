145cd33922d575a911f2c23cc976e84bc08dcd0c
AlexMarinda <malalex44@gmail.com>
2019-07-15 21:13:45 +0200

    Add sign up functionality

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+<<<<<<< HEAD
 
 import express from 'express';
 
   const app = express();
 const port = process.env.PORT || 3000;
+=======
+import express from 'express';
+import bodyParser from 'body-parser';
+import routes from './routes';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+  const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const port = process.env.PORT || 3000;
+app.use(routes);
+>>>>>>> Initial commit
 app.use('*', (req, res) => res.status(404).send({
         status: 404,
     message: 'URL NOT FOUND!',
---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import user from './user';
+
+
+const router = express.Router();
+
+router.use('/auth', user);
+
+
+export default router;
